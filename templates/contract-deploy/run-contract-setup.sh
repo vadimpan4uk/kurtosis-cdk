959234b7247ed29326da95f39097307efcaac053
Vadym Panchuk <vadimpan4uk@gmail.com>
2024-08-15 21:25:26 +0100

    default vals

--- templates/contract-deploy/run-contract-setup.sh
+++ templates/contract-deploy/run-contract-setup.sh
@@ -87,9 +87,9 @@ echo_ts "Step 2: Creating genesis"
 cp /opt/contract-deploy/1_createGenesis.ts deployment/v2/1_createGenesis.ts
 echo_ts "Step 2: Creating genesis(replaced file)"
 NODE_OPTIONS="--max-old-space-size=20480000" MNEMONIC="{{.l1_preallocated_mnemonic}}" npx ts-node deployment/v2/1_createGenesis.ts \ 
-    --genesisAdddress='{{.genealogy_contract_address}}' \
-    --storageUrl='{{.genesis_storage_url}}' \
-    --rpcUrl='{{.l1_rpc_url}}' \
+    --genesisAdddress="{{.genealogy_contract_address}}" \
+    --storageUrl="{{.genesis_storage_url}}" \
+    --rpcUrl="{{.l1_rpc_url}}" \
     --predefined='[{"isContract": false,"name": "admin","balance": "100000000000000000000000","address": "0x02d655B0396e40dA9fD80C0528f9C3fb488895Df"},{"isContract": true,"name": "GENEALOGY","balance": "0","address": "0x0C8542AB89c1C60D711B00F309f7EF63b5D9d6eb"}]' | tee 02_create_genesis.out
 
 echo_ts "Step 3: Deploying PolygonZKEVMDeployer"
