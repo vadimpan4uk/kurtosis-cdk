cb55c6c08fd9d4904efd1664e33adbb23ee3e2ed
Vadym Panchuk <vadimpan4uk@gmail.com>
2024-08-15 19:18:09 +0100

    zkevm folder param

--- templates/get-rollup-info2.sh
+++ templates/get-rollup-info2.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 
-curl -O https://daisy-dev-prepopulate.s3.eu-central-1.amazonaws.com/biolimitless/zkevm/combined.json
+curl -O {{.zkevm_folder}}/combined.json
 cp combined.json /opt/zkevm/combined.json
 
-curl -O https://daisy-dev-prepopulate.s3.eu-central-1.amazonaws.com/biolimitless/genesis.json
+curl -O {{.zkevm_folder}}/genesis.json
 cp genesis.json /opt/zkevm/genesis.json
 
-curl -O https://daisy-dev-prepopulate.s3.eu-central-1.amazonaws.com/biolimitless/zkevm/agglayer.keystore
+curl -O {{.zkevm_folder}}/agglayer.keystore
 cp agglayer.keystore /opt/zkevm/agglayer.keystore
-curl -O https://daisy-dev-prepopulate.s3.eu-central-1.amazonaws.com/biolimitless/zkevm/claimtxmanager.keystore
+curl -O {{.zkevm_folder}}/claimtxmanager.keystore
 cp claimtxmanager.keystore /opt/zkevm/claimtxmanager.keystore
-curl -O https://daisy-dev-prepopulate.s3.eu-central-1.amazonaws.com/biolimitless/zkevm/sequencer.keystore
+curl -O {{.zkevm_folder}}/sequencer.keystore
 cp sequencer.keystore /opt/zkevm/sequencer.keystore
-curl -O https://daisy-dev-prepopulate.s3.eu-central-1.amazonaws.com/biolimitless/zkevm/aggregator.keystore               
+curl -O {{.zkevm_folder}}/aggregator.keystore               
 cp aggregator.keystore /opt/zkevm/aggregator.keystore
-curl -O https://daisy-dev-prepopulate.s3.eu-central-1.amazonaws.com/biolimitless/zkevm/proofsigner.keystore
+curl -O {{.zkevm_folder}}/proofsigner.keystore
 cp proofsigner.keystore /opt/zkevm/proofsigner.keystore                      
-curl -O https://daisy-dev-prepopulate.s3.eu-central-1.amazonaws.com/biolimitless/zkevm/dac.keystore
+curl -O {{.zkevm_folder}}/dac.keystore
 cp dac.keystore /opt/zkevm/dac.keystore
