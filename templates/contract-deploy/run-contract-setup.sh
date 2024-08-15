4689ed28186aa5db654e730099388f4a168e713f
Vadym Panchuk <vadimpan4uk@gmail.com>
2024-08-15 21:49:02 +0100

    default vals

--- templates/contract-deploy/run-contract-setup.sh
+++ templates/contract-deploy/run-contract-setup.sh
@@ -86,12 +86,7 @@ npx hardhat run deployment/testnet/prepareTestnet.ts --network localhost | tee 0
 echo_ts "Step 2: Creating genesis"
 cp /opt/contract-deploy/1_createGenesis.ts deployment/v2/1_createGenesis.ts
 echo_ts "Step 2: Creating genesis(replaced file)"
-echo_ts "--------- {{.genesis_storage_url}} --------- {{.genealogy_contract_address}} ----- {{.args.zkevm_da_image}}"
-NODE_OPTIONS="--max-old-space-size=20480000" MNEMONIC="{{.l1_preallocated_mnemonic}}" npx ts-node deployment/v2/1_createGenesis.ts \ 
-    --genesisAdddress="{{.genealogy_contract_address}}" \
-    --storageUrl="{{.genesis_storage_url}}" \
-    --rpcUrl="{{.l1_rpc_url}}" \
-    --predefined='[{"isContract": false,"name": "admin","balance": "100000000000000000000000","address": "0x02d655B0396e40dA9fD80C0528f9C3fb488895Df"},{"isContract": true,"name": "GENEALOGY","balance": "0","address": "{{.genealogy_contract_address}}"}]' | tee 02_create_genesis.out
+NODE_OPTIONS="--max-old-space-size=20480000" MNEMONIC="{{.l1_preallocated_mnemonic}}" npx ts-node deployment/v2/1_createGenesis.ts --genesisAdddress="{{.genealogy_contract_address}}" --storageUrl="{{.genesis_storage_url}}" --rpcUrl="{{.l1_rpc_url}}" --predefined='[{"isContract": false,"name": "admin","balance": "100000000000000000000000","address": "0x02d655B0396e40dA9fD80C0528f9C3fb488895Df"},{"isContract": true,"name": "GENEALOGY","balance": "0","address": "{{.genealogy_contract_address}}"}]' | tee 02_create_genesis.out
 
 echo_ts "Step 3: Deploying PolygonZKEVMDeployer"
 npx hardhat run deployment/v2/2_deployPolygonZKEVMDeployer.ts --network localhost | tee 03_zkevm_deployer.out
